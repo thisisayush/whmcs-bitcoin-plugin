04ed4cae353b07d49ff0e51e7d6eae6570e12b5e
Darren <darrenwestwood86@gmail.com>
2020-03-26 22:24:05 +0200

    Remove flyp_id column if exists

--- modules/gateways/Blockonomics/Blockonomics.php
+++ modules/gateways/Blockonomics/Blockonomics.php
@@ -337,6 +337,11 @@ class Blockonomics {
 					$table->string('blockonomics_currency');
 				 });
 			}
+			if(!Capsule::schema()->hasColumn('blockonomics_bitcoin_orders', 'flyp_id')){
+			    Capsule::schema()->table('blockonomics_bitcoin_orders', function($table){
+			        $table->dropColumn('flyp_id');
+			    });
+			}
 		}
 	}
 
