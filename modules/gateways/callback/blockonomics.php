39ac5bc1ca2af83b33a3378a56593cd993e2063a
Darren <darrenwestwood86@gmail.com>
2020-09-22 12:57:50 +0200

    Convert received amount to order currency

--- modules/gateways/callback/blockonomics.php
+++ modules/gateways/callback/blockonomics.php
@@ -81,6 +81,7 @@ if($value < $bits - $underpayment_slack) {
 	$paymentAmount = $order['value'];
 }
 
+$paymentAmount = $blockonomics->convertAmountToOrderCurrency($order, $paymentAmount);
 $blockonomics->updateInvoiceNote($invoiceId, null);
 $blockonomics->updateOrderInDb($addr, $txid, $status, $value);
 
