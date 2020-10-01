0065747a2cf9403165e9523f22eabd3961a565d2
jusasiiv <juha@siivikko.fi>
2020-10-01 03:40:57 -0700

    Increase version number

--- modules/gateways/Blockonomics/Blockonomics.php
+++ modules/gateways/Blockonomics/Blockonomics.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 
 class Blockonomics {
 
-	private $version = '1.5.1';
+	private $version = '1.8.0';
 
 	/*
 	 * Get the blockonomics version
