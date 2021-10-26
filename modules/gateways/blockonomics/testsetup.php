8406f468c68ab912450fc2832feabaeca43851d1
Ayush Agarwal <thisisayushaa@gmail.com>
2021-10-26 05:45:16 -0400

    (refactor) Remove redundant code

--- modules/gateways/blockonomics/testsetup.php
+++ modules/gateways/blockonomics/testsetup.php
@@ -5,18 +5,6 @@ require_once __DIR__ . '/blockonomics.php';
 
 use Blockonomics\Blockonomics;
 
-    
 $blockonomics = new Blockonomics();
-
-$response = array();
-$error = array();
-
 $error = $blockonomics->testSetup();
-
-if (isset($error) && count($error) != 0) {
-    if (count(array_filter($error, function($item) { return $item != false; })) != 0) {
-        $response = $error;
-    }
-}
-
-echo json_encode($response);
+echo json_encode($error);
